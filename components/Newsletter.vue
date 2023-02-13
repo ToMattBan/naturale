cd78e74722d5ecff625618e3982af6d1a887a656
ToMattBan <souaquelecleyton@yandex.com>
2023-02-13 16:36:05 -0300

    Creating footer (WIP)

---
+++ components/Newsletter.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <form class="form-newsletter">
+      <input type="text" required/>
+      <input type="email" required/>
+      <input type="phone" required/>
+      <p>Campos com * são obrigatórios</p>
+      <button>Cadastrar</button>
+    </form>
+  </article>
+</template>
+
+<style scoped>
+.form-newsletter input:not(:last-of-type) {
+  width: 100%;
+}
+</style>
