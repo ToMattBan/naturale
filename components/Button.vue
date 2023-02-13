41e9e6494d330a78e9480cc5547dde0f3b39dda3
ToMattBan <souaquelecleyton@yandex.com>
2023-02-13 17:21:29 -0300

    Component button created and footer visually done

---
+++ components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button :class="`button ${variation}`" :type="type">{{ text }}</button>
+</template>
+
+<script setup lang="ts">
+type TButtonType = "button" | "submit" | "reset" | undefined;
+type TButtonVariation = "default" | "inverted";
+
+defineProps({
+  text: { type: String, required: true },
+  type: {
+    type: String as PropType<TButtonType>,
+    default: 'submit'
+  },
+  variation: {
+    type: String as PropType<TButtonVariation>,
+    default: 'default',
+  }
+})
+</script>
+
+<style scoped>
+button.button {
+  border-radius: 16px;
+  border: 0;
+  text-align: center;
+  padding: 4px;
+  font-weight: 700;
+  font-size: 1.1rem;
+  cursor: pointer;
+}
+
+button.button.default {
+  color: white;
+  background-color: var(--green);
+}
+
+button.button.inverted {
+  color: var(--green);
+  background-color: white;
+}
+</style>
